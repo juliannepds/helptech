feef8d1f872f7bf40c7b5940b8b35114ddeb9a3a
jaineRodrigues <jainer469@gmail.com>
2023-10-01 00:32:20 -0300

    Adicionando estrutura inicial da pagina

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
