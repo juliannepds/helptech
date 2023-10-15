f1e6c65569a1ab3ca0d276703e0423ab83ae3856
jaineRodrigues <jainer469@gmail.com>
2023-10-15 09:46:13 -0300

    Adicionando logica para mostrar os cards das tecnologias

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+function mostrarCards(tecnologia) {
+    var cards = document.querySelectorAll('.plano-estudo');
+    cards.forEach(function(card) {
+        if (card.id === tecnologia) {
+            if (card.classList.contains('show')) {
+                card.classList.remove('show');
+            } else {
+                card.classList.add('show');
+            }
+        } else {
+            card.classList.remove('show');
+        }
+    });
+}
+
