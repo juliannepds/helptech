fdc1508e755c57dc7405654c34dd10603159a895
jaineRodrigues <jainer469@gmail.com>
2023-10-15 01:36:57 -0300

    Removendo codigo não utilizado no script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,4 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
 }
